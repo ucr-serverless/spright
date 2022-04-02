19bfda3920fd8d9af1e98f36dc37d0ef86984d8f
Leslie Monis <lmoni004@ucr.edu>
2022-04-02 11:27:57 -0700

    fixup! Add Go NF

--- go/cmd/nf/main.go
+++ go/cmd/nf/main.go
@@ -4,16 +4,29 @@
 package main
 
 // #cgo pkg-config: libdpdk
+// #cgo CFLAGS: -I${SRCDIR}/../../../src/include
+// #cgo LDFLAGS: -L${SRCDIR}/../../../src
+// #cgo rte_ring LDFLAGS: -l:io_rte_ring.o
+// #cgo sk_msg LDFLAGS: -l:io_sk_msg.o -lbpf
 //
+// #include <errno.h>
+// #include <stdint.h>
+// #include <stdio.h>
 // #include <stdlib.h>
+// #include <string.h>
 //
+// #include <rte_branch_prediction.h>
 // #include <rte_eal.h>
 // #include <rte_errno.h>
 // #include <rte_memzone.h>
 //
-// static char **argv_create(int argc)
+// #include "http.h"
+// #include "io.h"
+// #include "spright.h"
+//
+// static void *argv_create(int argc)
 // {
-// 	char **argv = NULL;
+// 	char *argv = NULL;
 //
 // 	argv = malloc(argc * sizeof(char *));
 // 	if (argv == NULL) {
@@ -24,40 +37,124 @@ package main
 //	return argv;
 // }
 //
-// static void argv_destroy(char **argv)
+// static void argv_destroy(void *argv)
 // {
 // 	free(argv);
+// }
+//
+// static int nf_init(int argc, char **argv)
+// {
+// 	const struct rte_memzone *memzone = NULL;
+// 	int ret;
+//
+// 	ret = rte_eal_init(argc, argv);
+// 	if (unlikely(ret == -1)) {
+// 		fprintf(stderr, "rte_eal_init() error: %s\n",
+// 		        rte_strerror(rte_errno));
+// 		goto error_0;
+// 	}
+//
+// 	argc -= ret;
+// 	argv += ret;
+//
+// 	if (unlikely(argc == 1)) {
+// 		fprintf(stderr, "Network Function ID not provided\n");
+// 		goto error_1;
+// 	}
+//
+// 	errno = 0;
+// 	node_id = strtol(argv[1], NULL, 10);
+// 	if (unlikely(errno != 0 || node_id < 1)) {
+// 		fprintf(stderr, "Invalid value for Network Function ID\n");
+// 		goto error_1;
+// 	}
+//
+// 	memzone = rte_memzone_lookup(MEMZONE_NAME);
+// 	if (unlikely(memzone == NULL)) {
+// 		fprintf(stderr, "rte_memzone_lookup() error\n");
+// 		goto error_1;
+// 	}
+//
+// 	cfg = memzone->addr;
+//
+// 	ret = io_init();
+// 	if (unlikely(ret == -1)) {
+// 		fprintf(stderr, "io_init() error\n");
+// 		goto error_1;
+// 	}
+//
+// 	return 0;
+//
+// error_1:
+// 	rte_eal_cleanup();
+// error_0:
+// 	return -1;
+// }
+//
+// static int nf_exit(void)
+// {
+// 	int ret;
+//
+// 	ret = io_exit();
+// 	if (unlikely(ret == -1)) {
+// 		fprintf(stderr, "io_exit() error\n");
+// 		return -1;
+// 	}
+//
+// 	ret = rte_eal_cleanup();
+// 	if (unlikely(ret < 0)) {
+// 		fprintf(stderr, "rte_eal_cleanup() error: %s\n",
+// 		        rte_strerror(-ret));
+// 		return -1;
+// 	}
+//
+// 	return 0;
 // }
 import "C"
 
 import (
-	"fmt"
+	"errors"
 	"os"
+	"unsafe"
 )
 
-func init() {
+func IoInit() error {
 	argc := C.int(len(os.Args))
-	argv := C.argv_create(argc)
+	argv := (*[0xff]*C.char)(C.argv_create(argc))
+	defer C.argv_destroy(unsafe.Pointer(argv))
 
-	ret := C.rte_eal_init(argc, argv)
-	if (ret == -1) {
-		fmt.Fprintf(os.Stderr, "rte_eal_init() error: " +
-		                       C.GoString(C.rte_strerror(C.rte_errno)) +
-		                       "\n")
+	for i := 0; i < int(argc); i++ {
+		argv[i] = C.CString(os.Args[i])
+		defer C.free(unsafe.Pointer(argv[i]))
 	}
 
-	C.argv_destroy(argv)
+	ret := C.nf_init(argc, (**C.char)(unsafe.Pointer(argv)))
+	if (ret == -1) {
+		return errors.New("nf_init() error")
+	}
+
+	return nil
 }
 
-func cleanup() {
-	ret = C.rte_eal_cleanup()
+func IoExit() error {
+	ret := C.nf_exit()
 	if (ret < 0) {
-		fmt.Fprintf(os.Stderr, "rte_eal_cleanup() error: " +
-		                       C.GoString(C.rte_strerror(-ret)) + "\n")
+		return errors.New("nf_exit() error")
 	}
+
+	return nil
 }
 
 func main() {
-	init()
-	cleanup()
+	var err error
+
+	err = IoInit()
+	if err != nil {
+		panic(err)
+	}
+
+	err = IoExit()
+	if err != nil {
+		panic(err)
+	}
 }
