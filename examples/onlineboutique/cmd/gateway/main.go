9f934ed0129875241a07da997d451a1b0d56754f
Leslie Monis <lmoni004@ucr.edu>
2022-05-26 18:01:24 -0700

    fixup! Add examples/onlineboutique

--- examples/onlineboutique/cmd/gateway/main.go
+++ examples/onlineboutique/cmd/gateway/main.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 University of California, Riverside
+
 package main
 
 import (
--- examples/onlineboutique/internal/common/common.go
+++ examples/onlineboutique/internal/common/common.go
@@ -1,18 +1,21 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 University of California, Riverside
+
 package common
 
 type Transaction struct {
 }
 
 const (
-	NodeGateway uint8 = iota
-	NodeFrontend
-	NodeServiceAd
-	NodeServiceCart
-	NodeServiceCurrency
-	NodeServiceEmail
-	NodeServicePayment
-	NodeServiceProductCatalog
-	NodeServiceRecommendation
-	NodeServiceShipping
+	NodeIDGateway uint8 = iota
+	NodeIDFrontend
+	NodeIDServiceAd
+	NodeIDServiceCart
+	NodeIDServiceCurrency
+	NodeIDServiceEmail
+	NodeIDServicePayment
+	NodeIDServiceProductCatalog
+	NodeIDServiceRecommendation
+	NodeIDServiceShipping
 	TotalNodes
 )
